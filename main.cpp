4b606c0d6e81506110d045ec9b9faa22cd0fffb1
mhahmadilivany <mojtabaahmadi.redsmart@yahoo.com>
2013-06-01 16:42:33 +0430

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,11 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     welcome z;
     z.show();
-    /*tables ab("school.db","class1");
+    tables ab("school.db","class1");
     ab.table_completer(30);
+    ab.database_reader();
     tables b("class2");
-    b.table_completer(29);*/
+    b.database_reader();
+    b.table_completer(29);
     return a.exec();
 }
